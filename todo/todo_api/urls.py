a4b5ebaf6ad67e23f00b42401df4c5ef08b09aab
Robert Coffie <rcoffie22@yahoo.com>
2022-07-21 10:27:11 +0000

    add url refactoring

--- todo/todo_api/urls.py
+++ todo/todo_api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from todo.todo_api.views import ListTodo, UpdateTodo
 urlpatterns = [
 path('',ListTodo.as_view(),name='list_todo'),
-path('<int:pk>/',UpdateTodo.as_view(), name='update_todo')
+path('<int:pk>/',UpdateTodo.as_view(), name='update_todo_api')
 
 ]
